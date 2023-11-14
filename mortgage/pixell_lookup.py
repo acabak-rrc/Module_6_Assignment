15cafc98d542577b7eb573611590b34c6a5ad93b
Aidan Cabak <acabak@academic.rrc.ca>
2023-11-14 14:44:30 -0600

    Added MortgageRate and MortgageRequency Enums along with relevant documentation

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -14,3 +14,36 @@ from enum import Enum
 
 VALID_AMORTIZATION = [5, 10, 15, 20, 25, 30] 
 
+class MortgageRate(Enum):
+    """
+    An enumeration of the different mortage rate options.
+
+    Values:
+    - FIXED_5: a fixed-rate mortgage
+    - FIXED_3: a fixed-rate mortgage
+    - FIXED_1: a fixed-rate mortgage
+    - VARIABLE_5: a variable-rate mortgage
+    - VARIABLE_3: a variable-rate mortgage
+    - VARIABLE_1: a variable-rate mortgage
+    """
+
+    FIXED_5 = 0.0500
+    FIXED_3 = 0.0579
+    FIXED_1 = 0.0589
+    VARIABLE_5 = 0.0650
+    VARIABLE_3 = 0.0660
+    VARIABLE_1 = 0.0679
+
+class MortgageFrequency(Enum):
+    """
+    An enumeration of different mortgage frequencies.
+
+    Values:
+    - MONTHLY: a monthly mortgage
+    - BI_WEEKLY: a bi-weekly mortgage
+    - WEEKLY: a weekly mortgage
+    """
+
+    MONTHLY = 12
+    BI_WEEKLY = 26
+    WEEKLY = 52
