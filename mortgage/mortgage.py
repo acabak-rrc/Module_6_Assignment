0cff62bf76d0b89b6c39b552873c068aaed818d7
Aidan Cabak <acabak@academic.rrc.ca>
2023-11-14 14:32:22 -0600

    Update project documentation and README

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -1,7 +1,7 @@
 """
 Description: A class meant to manage Mortgage options.
-Author: {Student Name}
-Date: {Date}
+Author: Aidan Cabak
+Date: 2023-11-14
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -1,7 +1,7 @@
 """
 Description: A class used to test the Mortgage class.
-Author: {Student Name}
-Date: {Date}
+Author: Aidan Cabak
+Date: 2023-11-14
 Usage: Use the tests encapsulated within this class to test the MortgagePayment class.
 """
 
